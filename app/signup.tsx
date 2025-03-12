f616fdaba95aaaa483bd76c350db4da4c26d0bfe
Raymond Goode <raymond@rggstudio.com>
2025-03-12 07:17:57 -0400

    Update TypeScript configuration and signup form handling

--- app/signup.tsx
+++ app/signup.tsx
@@ -69,7 +69,7 @@ export default function SignUp() {
       
       // Launch image picker
       const result = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        mediaTypes: 'images',
         allowsEditing: true,
         aspect: [1, 1],
         quality: 0.8,
@@ -140,7 +140,7 @@ export default function SignUp() {
         firstName,
         lastName,
         profileImage,
-        circleOption,
+        circleOption || undefined,
         familyCircleName,
         inviteCode
       );
